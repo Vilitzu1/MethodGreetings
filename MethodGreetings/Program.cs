df42e0f7dd9d56cb00e884bdc05ccb7047210440
vilif <koskenkorva21@gmail.com>
2023-11-29 19:30:53 +0200

    Add project files.

---
+++ MethodGreetings/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace MethodGreetings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string friend1 = "Heidi";
+            string friend2 = "Juha";
+            string friend3 = "Niina";
+
+            GreetFriends(friend1);
+            GreetFriends(friend2);
+            GreetFriends(friend3);
+        }
+        public static void GreetFriends(string friendName) 
+        { 
+            Console.WriteLine("Hi " + friendName + ", my friend!");
+        }
+    }
+}
